38035044228d9d53cc3e722b98c7c92e8aee684f
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-06-05 00:30:38 +0200

    index js is modified

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import loadApp from './examples/menu/app.js';
-// import loadApp from './examples/pokemons/app.js';
-// import loadApp from './examples/currency-converter/app.js';
-// import loadApp from './examples/stopwatch/app.js';
-// import loadApp from './examples/github/app.js';
-// import loadApp from './examples/nobelprize/app.js';
-// import loadApp from './app.js';
+// import loadApp from "./examples/menu/app.js";
+// import loadApp from "./examples/pokemons/app.js";
+// import loadApp from "./examples/currency-converter/app.js";
+// import loadApp from "./examples/stopwatch/app.js";
+// import loadApp from "./examples/github/app.js";
+// import loadApp from "./examples/nobelprize/app.js";
+import loadApp from "./app.js";
 
-window.addEventListener('load', loadApp);
+window.addEventListener("load", loadApp);
