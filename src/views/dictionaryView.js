32a6f7cbfaa0a8b15e6d553433fd61f8ec76cd63
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-06-05 09:21:55 +0200

    view is modified and now works for synonyms and athonyms

--- src/views/dictionaryView.js
+++ src/views/dictionaryView.js
@@ -1,4 +1,5 @@
 function createHomeView(props) {
+  let selectedSearchType = "definitions";
   const root = document.createElement("div");
   root.innerHTML = String.raw`
     <header class="header">
@@ -8,55 +9,81 @@ function createHomeView(props) {
     </header>
     <div class="content-container whiteframe">
     <form action="" id="search-form">
+    <select id="search-dropdown-list">
+          <option selected value="definitions">Search Definitions</option>
+          <option value="synonyms">Search Synonyms</option>
+          <option value="antonyms">Search Antonyms</option>
+        </select>
     <input type="text" id="word-input" name="word">
     <input type="submit" value="Search">
   </form>
-  <div class="button-container"> 
-      <button id="synonyms" class="hide">Synonyms</button>
-      <button id="antonyms" class="hide">Antonyms</button>
-      </div >
     </div>
     <div class="display-container hide"></div>`;
   const displayContainer = root.querySelector(".display-container");
+  const dropdownList = root.querySelector("#search-dropdown-list");
+  dropdownList.addEventListener("change", (e) => {
+    selectedSearchType = e.target.value;
+  });
 
   const searchForm = root.querySelector("#search-form");
-  searchForm.addEventListener("submit", props.searchDefinitions);
+  searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    props.searchDefinitions(selectedSearchType);
+  });
 
-  const antonymsBtn = root.querySelector("#antonyms");
-  antonymsBtn.addEventListener("click", props.listAntonyms);
+  // const antonymsBtn = root.querySelector("#antonyms");
+  // antonymsBtn.addEventListener("click", props.listAntonyms);
 
-  const synonymsBtn = root.querySelector("#synonyms");
-  synonymsBtn.addEventListener("click", props.listSynonyms);
+  // const synonymsBtn = root.querySelector("#synonyms");
+  // synonymsBtn.addEventListener("click", props.listSynonyms);
 
   const update = (state) => {
     if (state.loading) {
-      antonymsBtn.classList.add("hide");
-      synonymsBtn.classList.add("hide");
       displayContainer.classList.add("hide");
 
       return;
     }
 
-    antonymsBtn.classList.remove("hide");
-    synonymsBtn.classList.remove("hide");
     displayContainer.classList.remove("hide");
 
     if (state.error) {
       displayContainer.textContent = state.error.message;
       return;
     }
-    console.log(`state.definitions ${state.definitions}`);
-    if (state.definitions && state.error === null) {
-      const arrayOfDefinitions = state.definitions;
-      let input = "";
 
-      arrayOfDefinitions.forEach((element) => {
-        const { partOfSpeech, definitions } = element;
-        input += `<h3> <span>${partOfSpeech} </span></h3>`;
-        for (const definition of definitions) {
-          input += `<p> ${definition.definition}</p>`;
+    if (state.searchResults && state.error === null) {
+      const { word, audio, searchType, ...rest } = state.searchResults;
+      let input = `<h2>${word}</h2> <br>
+                   `;
+      if (audio) {
+        input += `<audio controls>
+                  <source src=${audio} type="audio/mpeg">
+                  Your browser does not support the audio tag.
+                </audio>`;
+      }
+      if (searchType !== "definitions") {
+        const title = searchType.toUpperCase();
+        input += `<h3> <span>${title} </span></h3>`;
+      }
+      for (const element of rest.arrayOfSearchResults) {
+        console.log(`element ${element}`);
+
+        const { partOfSpeech, searchResults } = element;
+        console.log("search Results");
+        console.log(searchResults);
+        if (searchType !== "definitions") {
+          searchResults.forEach(
+            (searchResult) => (input += `<p> ${searchResult}</p>`)
+          );
+        } else {
+          input += `<h3> <span>${partOfSpeech} </span></h3>`;
+          for (const searchResult of searchResults) {
+            const data = searchResult["definition"];
+            input += `<p> ${data}</p>`;
+          }
         }
-      });
+      }
+
       displayContainer.innerHTML = input;
     }
   };
