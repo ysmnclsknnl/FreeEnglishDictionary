4c28fe9800383dd9c4bbb02b75a378846db6c180
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-06-07 11:21:42 +0200

    state object is modified

--- src/pages/dictionaryPage.js
+++ src/pages/dictionaryPage.js
@@ -5,13 +5,18 @@ const BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/en";
 async function fetchData(url) {
   const response = await fetch(url);
   if (!response.ok) {
-    throw new Error("Definition is not found");
+    throw new Error("HTTP Error: Page Is Not Found");
   }
   return response.json();
 }
 
 function createDictionaryPage() {
-  let state = { searchInput: "", selectedSearchType: "definitions" };
+  let state = {
+    searchInput: "",
+    selectedSearchType: "definitions",
+    searchResults: null,
+    error: null,
+  };
 
   const updateState = (updates) => {
     state = { ...state, ...updates };
@@ -19,21 +24,28 @@ function createDictionaryPage() {
     view.update(state);
   };
 
-  const onSearchInput = (e) => updateState({ searchInput: e.target.value });
+  //Search input is entered update the searchInput state and reset SearchResults state
+  const onSearchInput = (e) =>
+    updateState({ searchInput: e.target.value, searchResults: null });
+
+  //Search Type is selected update the selectedSearchType state
   const onSelectSearchType = (e) =>
     updateState({ selectedSearchType: e.target.value });
 
   const search = async (e) => {
     e.preventDefault();
+
     const word = state.searchInput;
     // Set the loading state and reset the error state.
     updateState({ loading: true, error: null });
 
     try {
       const results = await fetchData(`${BASE_URL}/${word} `);
+      console.log(results);
 
       const resultObject = {
         word: word,
+        phonetic: null,
         audio: null,
         searchType: state.selectedSearchType,
         arrayOfSearchResults: [],
@@ -41,6 +53,8 @@ function createDictionaryPage() {
 
       for (const result of results) {
         const { searchType, arrayOfSearchResults } = resultObject;
+
+        resultObject.phonetic = result.phonetic;
 
         for (const meaning of result.meanings) {
           const { partOfSpeech } = meaning;
